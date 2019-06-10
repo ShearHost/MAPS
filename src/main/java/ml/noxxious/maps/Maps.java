e4d18ed7f07b909b97ce5a0a55704dc069c77836
notyou12 <matanslevy6@gmail.com>
2019-06-09 21:01:30 -0400

    Added Startup Console Message

--- src/main/java/ml/noxxious/maps/Maps.java
+++ src/main/java/ml/noxxious/maps/Maps.java
@@ -1,10 +1,12 @@
 package ml.noxxious.maps;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Maps extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getServer().getConsoleSender().sendMessage(ChatColor.DARK_BLUE + "MAPS - Mineshaft Abuse Prevention System has started.");
     }
 }
